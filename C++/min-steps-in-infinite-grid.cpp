0aa8bb2c8c9302714997675c8c396b5b8edb1a4a
Rajat Gupta <58506865+rajatgupta2018@users.noreply.github.com>
2019-12-11 14:36:42 +0530

    Create min-steps-in-infinite-grid.cpp

---
+++ C++/min-steps-in-infinite-grid.cpp
@@ -0,0 +1,14 @@
+
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+    int dist=0;
+    
+    for (int i=0;i<A.size()-1;i++)
+    {
+        if( abs(A[i]-A[i+1]) <= abs(B[i]-B[i+1])    )
+            dist+=abs(B[i]-B[i+1]);
+        else
+            dist+=abs(A[i]-A[i+1]);
+    }
+    return dist;
+
+}
